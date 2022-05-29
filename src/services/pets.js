18531a3e0853df0282dc77ff73f6492dfcadde46
gsouza <guilherme5000sl@gmail.com>
2022-05-29 16:08:39 -0300

    Adicionado arquivo de configuração e modificado API code no serviço

--- src/services/pets.js
+++ src/services/pets.js
@@ -1,4 +1,5 @@
 import client from "../providers/client";
+import { apiCode } from "../config"
 
 export const listPets = () =>
-  client.get("pets/0rp4knn/a398510e-0800-47c2-94c1-29c990ac2bb");
+  client.get(`pets/${apiCode}`);
